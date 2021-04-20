11de8776a274f0744d7551a15b14b721eb3f35ef
Aleksey <atkachenko@techvice.org>
2021-04-20 19:09:31 +0300

    users

--- src/api/users.js
+++ src/api/users.js
@@ -4,25 +4,25 @@ export const createUserRequest = ({ data, token }) => {
   const { name, email, password, role } = data;
   return Request({
     method: "POST",
-    url: "/auth/users",
+    url: "/users",
     data: { name, email, password, role },
   }, token);
 };
 
-export const allUsersRequest = ({ params, token }) => {
+export const allUsersRequest = ({ params = {}, token }) => {
   const { name, role, sortBy = "desc", limit = 10, page = 1 } = params;
   return Request({
     method: "GET",
-    url: "/auth/users",
+    url: "/users",
     params: { name, sortBy, limit, role, page },
-  }, token);
+  }, localStorage.getItem("token"));
 };
 
 export const getUserByIdRequest = ({ params, token }) => {
   const { id } = params;
   return Request({
     method: "GET",
-    url: "/auth/users",
+    url: "/users",
     params: { id },
   }, token);
 };
@@ -32,7 +32,7 @@ export const updateUserByIdRequest = ({ params, data, token }) => {
   const { name, email, password } = data;
   return Request({
     method: "PATCH",
-    url: "/auth/users",
+    url: "/users",
     params: { id },
     data: { name, email, password },
   }, token);
@@ -42,7 +42,7 @@ export const removeUserByIdRequest = ({ params, token }) => {
   const { id } = params;
   return Request({
     method: "DELETE",
-    url: "/auth/users",
+    url: "/users",
     params: { id },
   }, token);
 };
--- src/pages/admin/Tables.jsx
+++ src/pages/admin/Tables.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 
 // components
 
 import CardTable from "components/Cards/CardTable";
+import CardTableUsers from "components/Cards/CardUsers";
 import { AdminTemplate } from "templates/admin-template";
+import { allUsersRequest } from "api/users";
 
 export const TablesPage = () => {
+  const [users, setUsers] = React.useState([]);
+  const fetchAllUsers = useCallback(async () => {
+    try {
+      const { data } = await allUsersRequest({
+        params: { sortBy: "desc", limit: 10, page: 1 },
+      });
+      setUsers(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  useEffect(() => {
+    fetchAllUsers();
+  }, [fetchAllUsers]);
   return (
     <AdminTemplate>
       <div className="flex flex-wrap mt-4">
@@ -15,7 +31,10 @@ export const TablesPage = () => {
         <div className="w-full mb-12 px-4">
           <CardTable color="dark" />
         </div>
+        <div className="w-full mb-12 px-4">
+          <CardTableUsers color="dark" data={users} />
+        </div>
       </div>
     </AdminTemplate>
   );
-}
+};
